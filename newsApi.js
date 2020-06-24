7c6a9725669a13367736920d188995e30931757d
Mattias Besemann <mattias.besemann@gmail.com>
2020-06-24 18:45:45 -0400

    Add basic news article retrieval

--- newsApi.js
+++ newsApi.js
@@ -5,19 +5,6 @@ $(document).ready(function() {
     getNews();
 })
 
-$(document).ready(function() {
-    getNews("sports");
-})
-
-$(document).ready(function() {
-    getNews("entertainment");
-
-    //Get favicons
-    $("a[href^='http']").each(function() {
-        $(this).prepend('<img src="https://www.google.com/s2/favicons?domain=' + this.href + '">');
-    });
-})
-
 // Category options are business, entertainment, general, health, science, sports, technology
 function getNews(category='') {
     $.ajax({
@@ -26,12 +13,13 @@ function getNews(category='') {
     }).then(function(response) {
         response.articles.forEach(story => {
             var articleDiv = $("<div>");
+            var favicon = $("<img>").prop("src", "https://www.google.com/s2/favicons?domain=" + story.url);
             var articleUrl = $("<a>").prop("href", story.url).text(story.title).prop("target", "_blank");
             var source = $("<h4>").text(story.source.name);
             var datePublished = $("<span>").text(story.publishedAt);
             var description = $("<p>").text(story.description);
-            articleDiv.append(articleUrl, source, datePublished, description);
-            $("body").append(articleDiv);
+            articleDiv.append(favicon, articleUrl, source, datePublished, description);
+            $(".news-articles-content").append(articleDiv);
         });
     });
 }
