7e0027aa83e3938dfafd70ecfd3f03fc4c4c944e
Kambiz Frounchi <kambiz.frounchi@gmail.com>
2020-06-24 20:21:17 -0400

    support for twitter API Fetch

--- twitterAPI.js
+++ twitterAPI.js
@@ -1,51 +1,26 @@
-var queryURL = "https://api.twitter.com/1.1/trends/place.json?";
-<<<<<<< HEAD
-var authParameters = "consumer_key=CvCZcL1YQpB5ndLhDnPATt784,consumer_secret=28Ap0upYFI3okcorEK0dcA5GYglpkGbxgym7NX0ClxAIHqqKtO, access_token=29120924-nxsRCe0q8AY1Q7SNlUUNGVKin3RP7UHrSjAbsyGv4, access_token_secret=60b31hGCBNRJo7lLRgDh9hKFbnkiUjFa5MeJzrRVpHfk8";
+var consumerKey = "n9tuklQ7vCvdyEjVm7Nts9uNc";
+var consumerSecretKey = "2GciZDmg4pDoOD8BfnKOsUUssqixf8my4FewOz0Cb1GHG1VHyq";
+var accessToken = "1275421014852337672-6mKsYvcidkbFWWzXTH8qS48vJSfwLM";
+var accessTokenSecret = "gXo35SEf2Rlsc4Dnc6LLMhJsTRb88kp0wBxHY9JRxxu6u";
 var queryParameters = "id=1";
 
-/*
-    consumer_key:         'CvCZcL1YQpB5ndLhDnPATt784',
-    consumer_secret:      '28Ap0upYFI3okcorEK0dcA5GYglpkGbxgym7NX0ClxAIHqqKtO',
-    access_token:         '29120924-nxsRCe0q8AY1Q7SNlUUNGVKin3RP7UHrSjAbsyGv4',
-    access_token_secret:  '60b31hGCBNRJo7lLRgDh9hKFbnkiUjFa5MeJzrRVpHfk8',
-*/
 
-var cb = new Codebird;
-cb.setConsumerKey("CvCZcL1YQpB5ndLhDnPATt784", "28Ap0upYFI3okcorEK0dcA5GYglpkGbxgym7NX0ClxAIHqqKtO");
-cb.setToken("29120924-nxsRCe0q8AY1Q7SNlUUNGVKin3RP7UHrSjAbsyGv4", "60b31hGCBNRJo7lLRgDh9hKFbnkiUjFa5MeJzrRVpHfk8");
-cb.__call("trends/place", {id: 1}, function(reply, rate, err) {
-    console.log(reply);
-    console.log(err);
-});
-
-/*
 $(document).ready(function () {
-    
-    var cb = new Codebird;
-    cb.setConsumerKey("CvCZcL1YQpB5ndLhDnPATt784", "28Ap0upYFI3okcorEK0dcA5GYglpkGbxgym7NX0ClxAIHqqKtO");
-    cb.setToken("29120924-nxsRCe0q8AY1Q7SNlUUNGVKin3RP7UHrSjAbsyGv4", "60b31hGCBNRJo7lLRgDh9hKFbnkiUjFa5MeJzrRVpHfk8");
-    cb.__call("trends/place", {id: 1}, function(reply, rate, err) {
+    var cb = new Codebird();
+    cb.setConsumerKey(consumerKey, consumerSecretKey);
+    cb.setToken(accessToken, accessTokenSecret);
+    cb.__call("trends/place", {id: 1}, function (reply, rate, err) {
         console.log(reply);
         console.log(err);
+        reply[0].trends.forEach(function(trend, index) {
+            console.log(trend);
+            var twitterTopicDiv = $("<div>");
+            var trendNameDiv = $("<div>").text("Trend: " + trend.name);
+            var trendURLDiv = $("<div>").text("URL: " + trend.url);
+            var trendTweetVolume = $("<div>").text("Tweet Volume: " + trend.tweet_volume);
+
+            twitterTopicDiv.append(trendNameDiv, trendURLDiv, trendTweetVolume);
+            $(".twitter-trends-content").append(twitterTopicDiv);
+        });
     });
-
-    
-    var query = queryURL + authParameters + queryParameters;
-    $.ajax({url: query, method: "GET"}).then(function(response) {
-        console.log(response);
-    });
-   
 });
-*/
-=======
-var queryParameters = "id=1";
-
-$(document).ready(function () {
-    
-    var query = queryURL + queryParameters;
-    $.ajax({url: query, method: "GET"}).then(function(response) {
-        console.log(response);
-    })
-
-});
->>>>>>> 366929b... twitter API: just console.log to start off
