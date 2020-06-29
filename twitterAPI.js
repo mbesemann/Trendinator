a857ec725f998d5ada4e4b80349a52108aa193b8
Kambiz Frounchi <kambiz.frounchi@gmail.com>
2020-06-29 19:32:01 -0400

    add geoLocation for twitter API

--- twitterAPI.js
+++ twitterAPI.js
@@ -21,42 +21,70 @@ var countryToWoeid =
 
 var cb = new Codebird();
 
-function fetchTwitterTrends(numItems) {
+function addNewElement(trend, count) {
+    var trendDiv = $("<div>");
+    trendDiv.addClass("twitter-article-item");
+    var trendNum = $("<div>").text(count);
+    var trendNameUrl = $("<a>").text(trend.name).prop("href", trend.url).prop("target", "_blank");
+    trendDiv.append(trendNum);
+    trendDiv.append(trendNameUrl);
+    trendDiv.append("<br>");
+    if (trend.tweet_volume) {
+        var trendTweetVolumeDiv = $("<div>").text("Tweet Volume: " + trend.tweet_volume);
+        trendDiv.append(trendTweetVolumeDiv);
+    }
+
+    twitterTrendsContentElement.append(trendDiv);
+}
+
+function fetchTwitterTrends(numItems, woeid) {
     cb.setConsumerKey(consumerKey, consumerSecretKey);
     cb.setToken(accessToken, accessTokenSecret);
-    cb.__call("trends/place", {id: canadaWoeid}, function (reply, rate, err) {
+    cb.__call("trends/place", {id: woeid}, function (reply, rate, err) {
         console.log(reply);
         console.log(err);
         var count = 0;
         reply[0].trends.some(function(trend, index) {
             if (count++ == numItems) {
-                console.log("count === numItems");
                 return true;
             }
-            var trendDiv = $("<div>");
-            trendDiv.addClass("twitter-article-item");
-            var trendNum = $("<div>").text(count);
-            var trendNameUrl = $("<a>").text(trend.name).prop("href", trend.url).prop("target", "_blank");
-            trendDiv.append(trendNum);
-            trendDiv.append(trendNameUrl);
-            trendDiv.append("<br>");
-            if (trend.tweet_volume) {
-                var trendTweetVolumeDiv = $("<div>").text("Tweet Volume: " + trend.tweet_volume);
-                trendDiv.append(trendTweetVolumeDiv);
-            }
 
-            twitterTrendsContentElement.append(trendDiv);
+            addNewElement(trend, count);
         });
     });    
+}
+
+function fetchTwitterTrendsLongLat(numItems, latitude, longitude) {
+    cb.setConsumerKey(consumerKey, consumerSecretKey);
+    cb.setToken(accessToken, accessTokenSecret);
+    cb.__call("trends/closest", {lat: latitude, long: longitude}, function (reply, rate, err) {
+        console.log(reply);
+        console.log(err);
+
+        var woeid = reply[0].woeid;
+        console.log(woeid);
+        fetchTwitterTrends(numItems, woeid); 
+    });
 }
 
 $(".ddl-item").on("click", function (event) {
     event.preventDefault();
     var numberOfItemsToRetrieve = parseInt($(this).text());
     twitterTrendsContentElement.empty();
-    fetchTwitterTrends(numberOfItemsToRetrieve);
+    fetchTwitterTrends(numberOfItemsToRetrieve, canadaWoeid);
 });
 
+function geoSuccess(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    fetchTwitterTrendsLongLat(defaultNumTrends, latitude, longitude);
+}
+
+function geoError() {
+    console.log("error getting the geolocation!")
+}
+
 $(document).ready(function () {
-    fetchTwitterTrends(defaultNumTrends);
+    fetchTwitterTrends(defaultNumTrends, canadaWoeid);
+    getGeoLocation(geoSuccess, geoError);
 });
